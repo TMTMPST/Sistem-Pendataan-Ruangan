bec48c5d9067b2232ff2063f7246b9d03e7ab19d
TMTMPST <vidoelag@gmail.com>
2023-09-12 09:45:09 +0700

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("_____________________________");
+        System.out.println("_____________________________");
+        System.out.println(" Pendataan Ruangan Di Gedung ");
+        System.out.println("             JTI             ");
+        System.out.println("    D-IV Teknik Informatika  ");
+        System.out.println("             2023            ");
+        System.out.println("_____________________________");
+        System.out.println("_____________________________");
+
+        
+
+    }
+}
