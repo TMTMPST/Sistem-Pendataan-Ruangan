cda42fcd5b3684c452d2ea14d3e6d583235e03c1
drm69 <yukihira14darma@gmail.com>
2023-09-19 12:34:11 +0700

    update

--- src/main.java
+++ src/main.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-
 public class main {
 
     public static void main(String[] args) {
 
-        Scanner input=new Scanner(System.in);
-        int pajak;
         System.out.println("====================================================================================================================");
         System.out.println("   _____ _____  _____ _______ ______ __  __   _____  ______ _____  _____            _         _  __          _   _ ");
         System.out.println("  / ____|_   _|/ ____|__   __|  ____|  \\/  | |  __ \\|  ____|  __ \\|  __ \\ /\\       | |  /\\   | |/ /    /\\   | \\ | |");
@@ -14,10 +11,25 @@ public class main {
         System.out.println("  ____) |_| |_ ____) |  | |  | |____| |  | | | |    | |____| | \\ \\| |  / ____ \\ |__| / ____ \\| . \\  / ____ \\| |\\  |");
         System.out.println(" |_____/|_____|_____/   |_|  |______|_|  |_| |_|    |______|_|  \\_\\_| /_/    \\_\\____/_/    \\_\\_|\\_\\/_/    \\_\\_| \\_|");
         System.out.println("====================================================================================================================");
-        
+Scanner input=new Scanner(System.in);
+int pajak;
+        System.out.println("_____________________________");
+        System.out.println("_____________________________");
+        System.out.println(" Selamat Datang di Kalkulator");
+        System.out.println("            Pajak            ");
+        System.out.println("    D-IV Teknik Informatika  ");
+        System.out.println("             2023            ");
+        System.out.println("_____________________________");
+        System.out.println("_____________________________");
+        System.out.println("=============================");
+        System.out.println("|Input  |Jenis Pajak        |");
+        System.out.println("| 1     | PPn               |");
+        System.out.println("| 2     | PBB               |");
+        System.out.println("| 3     | Pajak Kendaraan   |");
+        System.out.println("=============================");        
         System.out.print("Masukkan jenis pajak: ");
         pajak=input.nextInt();
-        
+
         if(pajak==1){
             Scanner sc= new Scanner(System.in);
             int gaji, tanggungan=4500000,PTKP, pribadi=54000000;
@@ -33,7 +45,30 @@ public class main {
             PTKP=(jmltanggungan*tanggungan)+pribadi;
             totpajak=(gaji-PTKP)*PPh;
             System.out.println("Total pajak anda: " + totpajak);
+        }else if(pajak==2){
+            Scanner scan=new Scanner(System.in);
+            int bumi, bangunan;
+            int luasbangunan, luasbumi;
+            int hargabumi,hargabangunan;
+            double tetapan1=0.2, tetapan2=12000000, PBB=0.005;
+            double NJOP, NJKP, totPBB;
+
+            System.out.print("Masukkan luas bangunan anda  :");
+            luasbangunan=scan.nextInt();
+            System.out.print("Masukkan luas tanah anda     :");
+            luasbumi=scan.nextInt();
+            System.out.print("Masukkan harga tanah/meter   :");
+            hargabumi=scan.nextInt();
+            System.out.print("Masukkan harga bangunan/meter:");
+            hargabangunan=scan.nextInt();
+            
+            bumi=luasbumi*hargabumi;
+            bangunan=luasbangunan*hargabangunan;
+            NJOP=bumi+bangunan;
+            NJKP=(NJOP-tetapan2)*tetapan1;
+            totPBB=NJKP*PBB;
+            System.out.println("Total pajak anda: " + totPBB);
+
         }
-        
     }
 }
