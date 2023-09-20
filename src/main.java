0b11f0ea6174907eb8081ce16047c101e378b9e1
drm69 <yukihira14darma@gmail.com>
2023-09-20 10:55:48 +0700

    update

--- src/main.java
+++ src/main.java
@@ -12,7 +12,7 @@ public class main {
         System.out.println(" |_____/|_____|_____/   |_|  |______|_|  |_| |_|    |______|_|  \\_\\_| /_/    \\_\\____/_/    \\_\\_|\\_\\/_/    \\_\\_| \\_|");
         System.out.println("====================================================================================================================");
 Scanner input=new Scanner(System.in);
-int pajak;
+        int pajak;
         System.out.println("_____________________________");
         System.out.println("_____________________________");
         System.out.println(" Selamat Datang di Kalkulator");
@@ -23,7 +23,7 @@ int pajak;
         System.out.println("_____________________________");
         System.out.println("=============================");
         System.out.println("|Input  |Jenis Pajak        |");
-        System.out.println("| 1     | PPn               |");
+        System.out.println("| 1     | PPh               |");
         System.out.println("| 2     | PBB               |");
         System.out.println("| 3     | Pajak Kendaraan   |");
         System.out.println("=============================");        
@@ -35,23 +35,41 @@ int pajak;
             int gaji, tanggungan=4500000,PTKP, pribadi=54000000;
             double PPh=0.05, totpajak;
             byte jmltanggungan;
+            String status;
 
-            System.out.println("masukkan gaji bersih selama 1 tahun:");
-            gaji=sc.nextInt();
-            System.out.println("masukkan jumlah tanggungan");
-            System.out.print("(anak + istri):");
-            jmltanggungan=sc.nextByte();
+            System.out.print("Apakah anda sudah menikah?: ");
+            status=sc.nextLine();
+            
+            if(status.equalsIgnoreCase("iya")){
+                System.out.println("masukkan gaji bersih selama 1 tahun:");
+                gaji=sc.nextInt();
+                System.out.println("masukkan jumlah tanggungan");
+                System.out.print("(anak + istri):");
+                jmltanggungan=sc.nextByte();
 
-            PTKP=(jmltanggungan*tanggungan)+pribadi;
-            totpajak=(gaji-PTKP)*PPh;
-            System.out.println("Total pajak anda: " + totpajak);
-        }else if(pajak==2){
+                PTKP=(jmltanggungan*tanggungan)+pribadi;
+                totpajak=(gaji-PTKP)*PPh;
+                System.out.println("Total pajak anda: " + totpajak);
+
+            }else {
+                System.out.println("masukkan gaji bersih selama 1 tahun:");
+                gaji=sc.nextInt();
+                System.out.println("masukkan jumlah tanggungan");
+                System.out.print("(anak angkat jika ada):");
+                jmltanggungan=sc.nextByte();
+
+                PTKP=(jmltanggungan*tanggungan)+pribadi;
+                totpajak=(gaji-PTKP)*PPh;
+                System.out.println("Total pajak anda: " + totpajak);
+                }
+
+    }else if(pajak==2){
             Scanner scan=new Scanner(System.in);
             int bumi, bangunan;
             int luasbangunan, luasbumi;
             int hargabumi,hargabangunan;
-            double tetapan1=0.2, tetapan2=12000000, PBB=0.005;
-            double NJOP, NJKP, totPBB;
+            double tetapan1=0.2, tetapan3=0.4, PBB=0.005;
+            double NJOP, NJKP, totPBB, nilaiaset, tetapan2=12000000;
 
             System.out.print("Masukkan luas bangunan anda  :");
             luasbangunan=scan.nextInt();
@@ -65,10 +83,20 @@ int pajak;
             bumi=luasbumi*hargabumi;
             bangunan=luasbangunan*hargabangunan;
             NJOP=bumi+bangunan;
-            NJKP=(NJOP-tetapan2)*tetapan1;
-            totPBB=NJKP*PBB;
-            System.out.println("Total pajak anda: " + totPBB);
+            nilaiaset=NJOP-tetapan2;
 
-        }
+            if(nilaiaset < 1000000000){
+                NJKP=nilaiaset*tetapan1;
+                totPBB=NJKP*PBB;
+                System.out.println("Total pajak anda: " + totPBB);
+            }else{
+                NJKP=nilaiaset*tetapan3;
+                totPBB=NJKP*PBB;
+                System.out.println("Total pajak anda: " + totPBB);
+            }
+    }else if(pajak==3){
+
     }
 }
+}
+
